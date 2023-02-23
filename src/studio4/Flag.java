2c37389c99a1338d07932fe54b04a8a55618a663
jasoncohen <jasoncohen@localhost>
2023-02-23 13:37:51 -0600

    Finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,26 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		
+		StdDraw.setPenColor(Color.orange);
+		StdDraw.filledRectangle(0.5, 0.7, 0.5, 0.7);
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.2);
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledRectangle(0.5, 1, 0.05, 1);
+		StdDraw.filledRectangle(1, 0.5, 1, 0.05);
+		
+		
+		
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,20 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String Shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double param1 = in.nextDouble();
+		double param2 = in.nextDouble();
+		double param3 = in.nextDouble();
+		double param4 = in.nextDouble();
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		StdDraw.ellipse(param1, param2, param3, param4);
+	
 		
 	}
 }
